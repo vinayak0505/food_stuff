35ae8b0228bdfa638686c36ce827dd963d97945c
Vinayak Agarwal <vinayak.agarwal@idreamcareer.com>
2022-07-15 09:29:19 +0530

    temprary changes

--- lib/src/data/model/autocomplete_recipe_search/autocomplete_recipe_search.dart
+++ lib/src/data/model/autocomplete_recipe_search/autocomplete_recipe_search.dart
@@ -22,17 +22,3 @@ abstract class AutocompleteRecipeSearch with _$AutocompleteRecipeSearch {
   factory AutocompleteRecipeSearch.fromJson(Map<String, dynamic> json) =>
       _$AutocompleteRecipeSearchFromJson(json);
 }
-
-// class EnumValues<T> {
-//   Map<String, T> map;
-//   Map<T, String> reverseMap;
-//
-//   EnumValues(this.map);
-//
-//   Map<T, String> get reverse {
-//     if (reverseMap == null) {
-//       reverseMap = map.map((k, v) => new MapEntry(v, k));
-//     }
-//     return reverseMap;
-//   }
-// }
--- lib/src/ui/home_detail_page/Components/ingredients_table.dart
+++ lib/src/ui/home_detail_page/Components/ingredients_table.dart
@@ -1,65 +1,65 @@
-import 'package:flutter/material.dart';
-import 'package:food_stuff/src/data/model/ingredient_model.dart';
-import 'package:food_stuff/src/data/model/recipe_information/res_recipe_info.dart';
-import 'package:food_stuff/src/utils/constants.dart';
+  import 'package:flutter/material.dart';
+  import 'package:food_stuff/src/data/model/ingredient_model.dart';
+  import 'package:food_stuff/src/data/model/recipe_information/res_recipe_info.dart';
+  import 'package:food_stuff/src/utils/constants.dart';
 
-class IngredientsTable extends StatefulWidget {
-  const IngredientsTable({Key? key, required this.ingredientList})
-      : super(key: key);
+  class IngredientsTable extends StatefulWidget {
+    const IngredientsTable({Key? key, required this.ingredientList})
+        : super(key: key);
 
-  final List<ExtendedIngredient> ingredientList;
+    final List<ExtendedIngredient> ingredientList;
 
-  @override
-  _IngredientsTableState createState() => _IngredientsTableState();
-}
-
-class _IngredientsTableState extends State<IngredientsTable> {
-  late List<Ingredient> myIngredientList;
-
-  Ingredient ingredientsData(ExtendedIngredient index) {
-    return Ingredient(
-        index.name ?? '', index.amount.toString(), index.unit ?? '');
+    @override
+    _IngredientsTableState createState() => _IngredientsTableState();
   }
 
-  @override
-  void initState() {
-    myIngredientList =
-        widget.ingredientList.map((e) => ingredientsData(e)).toList();
-    super.initState();
-  }
+  class _IngredientsTableState extends State<IngredientsTable> {
+    late List<Ingredient> myIngredientList;
 
-  @override
-  Widget build(BuildContext context) {
-    return GridView.count(
-      physics: const ScrollPhysics(),
-      shrinkWrap: true,
-      childAspectRatio: ((kScreenWidth(context) / 1) / 100),
-      crossAxisCount: 2,
-      children: List.generate(myIngredientList.length ~/ 2, (index) {
-        return Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 8),
-          child: Row(
-            children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 8),
-                child: Container(
-                  height: 8,
-                  width: 8,
-                  decoration: const BoxDecoration(
-                    color: Colors.orange,
-                    shape: BoxShape.circle,
+    Ingredient ingredientsData(ExtendedIngredient index) {
+      return Ingredient(
+          index.name ?? '', index.amount.toString(), index.unit ?? '');
+    }
+
+    @override
+    void initState() {
+      myIngredientList =
+          widget.ingredientList.map((e) => ingredientsData(e)).toList();
+      super.initState();
+    }
+
+    @override
+    Widget build(BuildContext context) {
+      return GridView.count(
+        physics: const ScrollPhysics(),
+        shrinkWrap: true,
+        childAspectRatio: ((kScreenWidth(context) / 1) / 100),
+        crossAxisCount: 2,
+        children: List.generate(myIngredientList.length ~/ 2, (index) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8),
+            child: Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 8),
+                  child: Container(
+                    height: 8,
+                    width: 8,
+                    decoration: const BoxDecoration(
+                      color: Colors.orange,
+                      shape: BoxShape.circle,
+                    ),
                   ),
                 ),
-              ),
-              const SizedBox(width: 3),
-              Flexible(
-                child: Text(
-                    '${myIngredientList[index].measure} ${myIngredientList[index].unit} ${myIngredientList[index].name}'),
-              ),
-            ],
-          ),
-        );
-      }),
-    );
+                const SizedBox(width: 3),
+                Flexible(
+                  child: Text(
+                      '${myIngredientList[index].measure} ${myIngredientList[index].unit} ${myIngredientList[index].name}'),
+                ),
+              ],
+            ),
+          );
+        }),
+      );
+    }
   }
-}
